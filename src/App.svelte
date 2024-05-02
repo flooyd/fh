a1851204eba2065a25790a816b9e409e4ee80d40
floyd <floydtjones@gmail.com>
2024-05-02 10:47:14 -0500

    add loading

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,8 @@
-<script lang='ts'>
-  import { onMount } from 'svelte';
-  import LoginModal from './components/LoginModal.svelte';
-  import Nav from './components/Nav.svelte';
-  import Posts from './components/Posts.svelte';
+<script lang="ts">
+  import { onMount } from "svelte";
+  import LoginModal from "./components/LoginModal.svelte";
+  import Nav from "./components/Nav.svelte";
+  import Posts from "./components/Posts.svelte";
   import {
     page,
     loginOrRegister,
@@ -14,22 +14,22 @@
     users,
     refresh,
     fetchUrl,
-  } from './stores';
-  import CreatePost from './components/CreatePost.svelte';
-  import ViewPost from './components/ViewPost.svelte';
-  import ProfileModal from './components/ProfileModal.svelte';
+  } from "./stores";
+  import CreatePost from "./components/CreatePost.svelte";
+  import ViewPost from "./components/ViewPost.svelte";
+  import ProfileModal from "./components/ProfileModal.svelte";
 
   let ready = false;
 
-  if (localStorage.getItem('user')) {
-    const userFromStorage = localStorage.getItem('user');
+  if (localStorage.getItem("user")) {
+    const userFromStorage = localStorage.getItem("user");
     if (userFromStorage) {
       $user = JSON.parse(userFromStorage);
     }
   }
 
-  if (window.location.href.includes('vercel')) {
-    $fetchUrl = 'https://fhnest.onrender.com';
+  if (window.location.href.includes("vercel")) {
+    $fetchUrl = "https://fhnest.onrender.com";
   }
 
   const fetchUsers = async () => {
@@ -56,21 +56,21 @@
     const voteTypesRes = await fetch(`${$fetchUrl}/voteTypes`);
     const voteTypesData = await voteTypesRes.json();
     $voteTypes = voteTypesData;
-  }
+  };
 
   const init = async () => {
-    if(!$user) return;
+    if (!$user) return;
     await fetchVoteTypes();
     await fetchUsers();
     await fetchPostsAndVotes();
-    if (window.location.pathname.includes('/viewPost/')) {
-      const postId = window.location.pathname.split('/viewPost/')[1];
+    if (window.location.pathname.includes("/viewPost/")) {
+      const postId = window.location.pathname.split("/viewPost/")[1];
       $currentPost = $posts.find((post: any) => post.id === parseInt(postId));
-      $page = 'viewPost';
+      $page = "viewPost";
     } else {
-      window.history.replaceState('posts', '', '/');
+      window.history.replaceState("posts", "", "/");
     }
-  }
+  };
 
   onMount(async () => {
     await init();
@@ -90,57 +90,61 @@
   $: $refresh ? init() : null;
 
   window.onpopstate = (e) => {
-    if(!e.state) return;
-    if (e.state === 'posts') {
-      $page = 'posts';
-      document.title = 'Forum House';
+    if (!e.state) return;
+    if (e.state === "posts") {
+      $page = "posts";
+      document.title = "Forum House";
     }
-    if(e.state.includes('viewPost')) {
-      const postId = e.state.split('/viewPost/')[1];
+    if (e.state.includes("viewPost")) {
+      const postId = e.state.split("/viewPost/")[1];
       $currentPost = $posts.find((post: any) => post.id === parseInt(postId));
-      $page = 'viewPost';
-      document.title = 'Forum House - ' + $currentPost.title;
+      $page = "viewPost";
+      document.title = "Forum House - " + $currentPost.title;
     }
-    if(e.state === 'createPost') {
-      $page = 'createPost';
-      document.title = 'Forum House - Create Post';
+    if (e.state === "createPost") {
+      $page = "createPost";
+      document.title = "Forum House - Create Post";
     }
-    if(e.state.includes('editPost')) {
-      const postId = e.state.split('/editPost/')[1];
+    if (e.state.includes("editPost")) {
+      const postId = e.state.split("/editPost/")[1];
       $currentPost = $posts.find((post: any) => post.id === parseInt(postId));
-      $page = 'editPost';
-      document.title = 'Forum House - Edit Post';
+      $page = "editPost";
+      document.title = "Forum House - Edit Post";
     }
-  }
+  };
 </script>
 
 <Nav />
-  <main>
-    {#if $loginOrRegister === 'login' || $loginOrRegister === 'register'}
-      <LoginModal />
-    {/if}
-    {#if $profileModalOpen}
-      <ProfileModal />
-    {/if}
-    {#if $page === 'createPost'}
-      <CreatePost />
-    {/if}
-    {#if $page === 'posts' && $user}
-      <Posts />
-    {/if}
-    {#if $page === 'viewPost' && $currentPost}
-      <ViewPost />
-    {/if}
-    {#if $page === 'editPost' && $currentPost}
-      <CreatePost post={$currentPost} isEdit={true} />
-    {/if}
-  </main>
-
+{#if ready}
+<main>
+  {#if $loginOrRegister === "login" || $loginOrRegister === "register"}
+    <LoginModal />
+  {/if}
+  {#if $profileModalOpen}
+    <ProfileModal />
+  {/if}
+  {#if $page === "createPost"}
+    <CreatePost />
+  {/if}
+  {#if $page === "posts" && $user}
+    <Posts />
+  {/if}
+  {#if $page === "viewPost" && $currentPost}
+    <ViewPost />
+  {/if}
+  {#if $page === "editPost" && $currentPost}
+    <CreatePost post={$currentPost} isEdit={true} />
+  {/if}
+</main>
+{/if}
+{#if !ready}
+  <h1>Loading...</h1>
+{/if}
 <style>
   main {
     background: #333;
     color: white;
-    font-family: 'Quattrocento Sans', sans-serif;
+    font-family: "Quattrocento Sans", sans-serif;
     padding: 20px;
     height: calc(100vh - 61px);
     overflow-y: auto;
